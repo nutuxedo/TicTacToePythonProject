36296dca99776fece5cc7edfad1bee4b06edf825
Rafael Jose <141491411+nutuxedo@users.noreply.github.com>
2024-06-10 07:43:54 +0800

    updated class TicTacToeGame

--- main/main.py
+++ main/main.py
@@ -4,6 +4,7 @@ from tkinter import font
 from typing import NamedTuple
 from itertools import cycle
 
+
 # Player class
 class Player(NamedTuple):
     label: str
@@ -16,11 +17,13 @@ class Move(NamedTuple):
     col: int
     label: str = ""
 
+
 BOARD_SIZE = 3
 DEFAULT_PLAYERS = (
     Player(label='X', color='red'),
     Player(label='O', color='yellow'),
 )
+
 
 # Game class here
 class TicTacToeGame:
@@ -34,11 +37,61 @@ class TicTacToeGame:
         self._winning_conditions = []
         self._setup_board()
 
+    def _setup_board(self):
+        self._current_moves = [
+            [Move(row, col) for col in range(self.board_size)]
+            for row in range(self.board_size)
+        ]
+        self._winning_conditions = self._get_winning_combinations()
+
+    def _get_winning_combinations(self):
+        rows = [
+            [(move.row, move.col) for move in row]
+            for row in self._current_moves
+        ]
+        columns = [list(col) for col in zip(*rows)]
+        first_diagonal = [row[i] for i, row in enumerate(rows)]
+        second_diagonal = [col[j] for j, col in enumerate(reversed(columns))]
+        return rows + columns + [first_diagonal, second_diagonal]
+
+    def is_valid_move(self, move):
+        row, col = move.row, move.col
+        move_was_not_played = self._current_moves[row][col].label == ''
+        no_winner = not self._has_winner
+        return no_winner and move_was_not_played
+
+    def process_move(self, move):
+        row, col = move.row, move.col
+        self._current_moves[row][col] = move
+        for combo in self._winning_conditions:
+            results = set(
+                self._current_moves[n][m].label
+                for n, m in combo
+            )
+            is_win = (len(results) == 1) and ("" not in results)
+            if is_win:
+                self._has_winner = True
+                self.winner_combo = combo
+                break
+
+    def toggle_player(self):
+        self.current_player = next(self._players)
+
+    def is_tied(self):
+        no_winner = not self._has_winner
+        played_moves = (
+            move.label for row in self._current_moves for move in row
+        )
+        return no_winner and all (played_moves)
+
+    def has_winner(self):
+        return self._has_winner
+
 # TicTacToeBoard class here - bulk of the interface
 class TicTacToeBoard(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.title('oTicToe') # The window title of the application
+        self.title('oTicToe')  # The window title of the application
         self._cells = {}
         self._create_board_display()
         self._create_board_grid()
