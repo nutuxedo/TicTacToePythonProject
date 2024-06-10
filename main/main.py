c6327aad512de29c8d46173c452342d75b5d746f
Rafael Jose <141491411+nutuxedo@users.noreply.github.com>
2024-06-11 03:47:46 +0800

    Revert "[v0.1.0] base game almost finished - added game logic and initialization to start game"
    This reverts commit b4fa05496f45222084aa6084e497dcdb830dea68.

--- main/main.py
+++ main/main.py
@@ -1,7 +1,143 @@
-# This should be something....
-import random
+# imports
+import tkinter as tk
+from tkinter import font
+from typing import NamedTuple
+from itertools import cycle
 
 
-print ("Hello World")
-name = input()
-print(name)
+# Player class
+class Player(NamedTuple):
+    label: str
+    color: str
+
+
+# Move class and its logic
+class Move(NamedTuple):
+    row: int
+    col: int
+    label: str = ""
+
+
+BOARD_SIZE = 3
+DEFAULT_PLAYERS = (
+    Player(label='X', color='red'),
+    Player(label='O', color='yellow'),
+)
+
+
+# Game class here
+class TicTacToeGame:
+    def __init__(self, players=DEFAULT_PLAYERS, board_size=BOARD_SIZE):
+        self.players = cycle(players)
+        self.board_size = board_size
+        self.current_player = next(self.players)
+        self.winner_combo = []
+        self._current_moves = []
+        self._has_winner = False
+        self._winning_conditions = []
+        self._setup_board()
+
+    def _setup_board(self):
+        self._current_moves = [
+            [Move(row, col) for col in range(self.board_size)]
+            for row in range(self.board_size)
+        ]
+        self._winning_conditions = self._get_winning_combinations()
+
+    def _get_winning_combinations(self):
+        rows = [
+            [(move.row, move.col) for move in row]
+            for row in self._current_moves
+        ]
+        columns = [list(col) for col in zip(*rows)]
+        first_diagonal = [row[i] for i, row in enumerate(rows)]
+        second_diagonal = [col[j] for j, col in enumerate(reversed(columns))]
+        return rows + columns + [first_diagonal, second_diagonal]
+
+    def is_valid_move(self, move):
+        row, col = move.row, move.col
+        move_was_not_played = self._current_moves[row][col].label == ''
+        no_winner = not self._has_winner
+        return no_winner and move_was_not_played
+
+    def process_move(self, move):
+        row, col = move.row, move.col
+        self._current_moves[row][col] = move
+        for combo in self._winning_conditions:
+            results = set(
+                self._current_moves[n][m].label
+                for n, m in combo
+            )
+            is_win = (len(results) == 1) and ("" not in results)
+            if is_win:
+                self._has_winner = True
+                self.winner_combo = combo
+                break
+
+    def toggle_player(self):
+        self.current_player = next(self._players)
+
+    def is_tied(self):
+        no_winner = not self._has_winner
+        played_moves = (
+            move.label for row in self._current_moves for move in row
+        )
+        return no_winner and all (played_moves)
+
+    def has_winner(self):
+        return self._has_winner
+
+# TicTacToeBoard class here - bulk of the interface
+class TicTacToeBoard(tk.Tk):
+    def __init__(self, game):
+        super().__init__()
+        self.title('oTicToe')  # The window title of the application
+        self._cells = {}
+        self._game = game
+        self._create_board_display()
+        self._create_board_grid()
+
+    def _create_board_display(self):
+        display_frame = tk.Frame(master=self)
+        display_frame.pack(fill=tk.X)
+        self.display = tk.Label(
+            master=display_frame,
+            text="Ready?",
+            font=font.Font(size=28, weight="bold"),
+        )
+        self.display.pack()
+
+    def _create_board_grid(self):
+        grid_frame = tk.Frame(master=self)
+        grid_frame.pack()
+        for row in range(self._game.board_size):
+            self.rowconfigure(row, weight=1, minsize=50)
+            self.columnconfigure(row, weight=1, minsize=75)
+            for col in range(self._game.board_size):
+                button = tk.Button(
+                    master=grid_frame,
+                    text="",
+                    font=font.Font(size=36, weight='bold'),
+                    fg='black',
+                    width=3,
+                    height=2,
+                    highlightbackground="lightblue",
+                )
+                self._cells[button] = (row, col)
+                button.grid(
+                    row=row,
+                    column=col,
+                    padx=5,
+                    pady=5,
+                    sticky='nsew'
+                )
+
+
+# Code initialization
+def main():
+    board = TicTacToeBoard()
+    board.mainloop()
+
+
+if __name__ == "__main__":
+    main()
